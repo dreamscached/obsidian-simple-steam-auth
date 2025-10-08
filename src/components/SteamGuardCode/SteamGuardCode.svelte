65193163579642bcc7424e5d2ceeb9284a960742
Herman S. <dreamscache.d@gmail.com>
2025-10-08 14:00:13 +0200

    Preallocate space (skeleton) to prevent layout shift before an actual code is generated

--- src/components/SteamGuardCode/SteamGuardCode.svelte
+++ src/components/SteamGuardCode/SteamGuardCode.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onDestroy, onMount } from "svelte";
 	import SteamTotp from "steam-totp";
+
 	import { getSettings } from "$lib/settings/settings.svelte";
 	import CopyButton from "$components/ui/CopyButton.svelte";
 
@@ -16,10 +17,16 @@
 	let pluginSettings = getSettings();
 	let updateInterval = $state<IntervalRef | undefined>();
 	let authCode = $state<string | undefined>();
+	let error = $state<unknown | undefined>();
 	let reveal = $state(false);
 
 	async function updateAuthCode() {
-		authCode = await SteamTotp.getAuthCode(sharedSecret);
+		try {
+			authCode = await SteamTotp.getAuthCode(sharedSecret);
+			error = undefined;
+		} catch (e: unknown) {
+			error = e;
+		}
 	}
 
 	onMount(async () => {
@@ -31,26 +38,35 @@
 		clearInterval(updateInterval);
 	});
 
-	function onCodeSpanMouseOver() {
-		reveal = true;
-	}
-
-	function onCodeSpanMouseLeave() {
-		if (!pluginSettings.showCodeByDefault) {
+	function setCodeRevealed(value: boolean) {
+		if (value) {
+			reveal = true;
+		} else if (!pluginSettings.showCodeByDefault) {
 			reveal = false;
 		}
 	}
 </script>
 
 <div class="sg-container">
+	{#if authCode && error === undefined}
+		{@render authCodeSpan()}
+	{:else if error !== undefined}
+		{@render errorSpan()}
+	{:else}
+		{@render skeleton()}
+	{/if}
+	{@render copyButton()}
+</div>
+
+{#snippet authCodeSpan()}
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
 	<!-- svelte-ignore a11y_no_static_element_interactions -->
 	<span
 		class={["sg-totp-code", { "sg-totp-code-hidden": !reveal }]}
-		onfocus={onCodeSpanMouseOver}
-		onblur={onCodeSpanMouseLeave}
-		onmouseover={onCodeSpanMouseOver}
-		onmouseleave={onCodeSpanMouseLeave}
+		onfocus={() => setCodeRevealed(true)}
+		onblur={() => setCodeRevealed(false)}
+		onmouseover={() => setCodeRevealed(true)}
+		onmouseleave={() => setCodeRevealed(false)}
 	>
 		{#if reveal || pluginSettings.showCodeByDefault}
 			{authCode}
@@ -58,10 +74,21 @@
 			*****
 		{/if}
 	</span>
+{/snippet}
+
+{#snippet errorSpan()}
+	<span class="sg-totp-code-error">ERROR</span>
+{/snippet}
+
+{#snippet skeleton()}
+	<span class="sg-totp-code">*****</span>
+{/snippet}
+
+{#snippet copyButton()}
 	{#if pluginSettings.showCopyButton}
-		<CopyButton text={`${authCode}`} />
+		<CopyButton text={`${authCode}`} disabled={error !== undefined} />
 	{/if}
-</div>
+{/snippet}
 
 <style>
 	.sg-container {
@@ -79,5 +106,13 @@
 
 	.sg-totp-code-hidden {
 		color: var(--text-muted);
+	}
+
+	.sg-totp-code-error {
+		font-family: var(--font-monospace);
+		color: var(--color-red);
+		display: inline-flex;
+		flex-direction: row;
+		align-items: center;
 	}
 </style>
--- src/components/ui/CopyButton.svelte
+++ src/components/ui/CopyButton.svelte
@@ -8,13 +8,13 @@ Simple copy button, used to copy text content on click.
 	import Icon from "./Icon.svelte";
 
 	interface Props {
-		/**
-		 * Text content to copy.
-		 */
+		/** Text content to copy. */
 		text: string;
+		/** Whether a button is enabled. */
+		disabled?: HTMLButtonElement["disabled"];
 	}
 
-	let { text }: Props = $props();
+	let { text, disabled }: Props = $props();
 
 	async function onclick() {
 		if (text) {
@@ -25,7 +25,7 @@ Simple copy button, used to copy text content on click.
 	}
 </script>
 
-<button class="sg-copy-btn" {onclick}>
+<button class="sg-copy-btn" {disabled} {onclick}>
 	<Icon icon="copy" />
 </button>
 
